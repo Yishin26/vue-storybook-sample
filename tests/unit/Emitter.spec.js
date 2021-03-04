4e2faf08f9bc0e7dbac247948e6f716026d8234d
Louise Lin <louise.lin.habook@gmail.com>
2021-03-04 10:56:34 +0800

    update 單元測試案例: 如何測試 emit 事件

---
+++ tests/unit/Emitter.spec.js
@@ -0,0 +1,24 @@
+import Emitter from '@/components/Emitter.vue'
+import { shallowMount } from '@vue/test-utils'
+
+describe('Emitter', () => {
+  it('emits an event with two arguments', () => {
+    const wrapper = shallowMount(Emitter)
+
+    wrapper.vm.emitEvent()
+
+    expect(wrapper.emitted().myEvent[0]).toEqual(['name', 'password'])
+  })
+
+  //在不加载组件的情况下测试事件
+  it('emits an event without mounting the component', () => {
+    const events = {}
+    const $emit = (event, ...args) => {
+      events[event] = [...args]
+    }
+
+    Emitter.methods.emitEvent.call({ $emit })
+
+    expect(events.myEvent).toEqual(['name', 'password'])
+  })
+})
